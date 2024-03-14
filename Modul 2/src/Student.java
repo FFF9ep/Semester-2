029378eb7c1c27c66b5f6b1f7b9d9cef963cb53a
fandi a <fandiardyan@example.com>
2024-03-14 23:52:09 +0700

    Modul 2

---
+++ Modul 2/src/Student.java
@@ -0,0 +1,61 @@
+public class Student {
+    private String name;
+    private String faculty;
+    private String nim;
+    private String programStudi;
+    App app;
+
+    public Student(String name, String faculty, String nim, String programStudi) {
+        this.name = name;
+        this.faculty = faculty;
+        this.nim = nim;
+        this.programStudi = programStudi;
+    }
+
+    // Getter dan setter untuk setiap atribut
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getProgramStudi() {
+        return programStudi;
+    }
+
+    public void setProgramStudi(String programStudi) {
+        this.programStudi = programStudi;
+    }
+
+    public void Logout() {
+        System.out.println("System logout...");
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        app.menu();
+    }
+
+    public void displayBook() {
+
+    }
+}
